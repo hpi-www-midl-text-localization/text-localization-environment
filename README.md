2ccad5f8e0368002d5040758b2aeafbad844ac0d
Jona Otholt <Jona.Otholt@student.hpi.uni-potsdam.de>
2018-11-20 22:25:26 +0100

    Add instance variables, reset() and render() functions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # text-localization-environment
 The environment we will use to train our text localization agent.
+
+For now, we will stick to the structure of the environment as laid out in the 
+[base paper](http://slazebni.cs.illinois.edu/publications/iccv15_active.pdf). We conform to the 
+[OpenAi](https://github.com/openai/gym)-Interface.
--- environment.py
+++ environment.py
@@ -1,10 +1,17 @@
 import gym
+from PIL import ImageDraw
 
 
 class TextLocEnv(gym.Env):
 
-    def __init__(self, image):
-        pass
+    def __init__(self, image, true_bboxes):
+        self.alpha = 0.2
+
+        self.image = image
+        self.true_bboxes = true_bboxes
+        self.history = []
+        self.bbox = (0, 0, image.width, image.height)
+        self.state = self.compute_state()
 
     def step(self, action):
         """Execute an action and return
@@ -12,12 +19,24 @@ class TextLocEnv(gym.Env):
             reward - the reward,
             done - whether a terminal state was reached,
             info - any additional info"""
+
         return 0, 0, True, {}
 
     def reset(self):
-        """Reset the environment to its initial state"""
-        pass
+        """Reset the environment to its initial state (the bounding box covers the entire image"""
+        self.history = []
+        self.bbox = (0, 0, self.image.width, self.image.height)
+        self.state = self.compute_state()
+
+        return self.state
 
     def render(self, mode='human'):
         """Render the current state"""
-        pass
+        copy = self.image.copy()
+        draw = ImageDraw.Draw(copy)
+        draw.rectangle(self.bbox, outline=(255, 255, 255))
+        copy.show()
+
+    def compute_state(self):
+        """Compute the state from the image, the bounding box and the action history"""
+        return 0
