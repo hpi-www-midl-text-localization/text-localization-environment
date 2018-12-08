a4c64dce04ed06082499d7fcb3685ca7a328308e
J-L-O <18723283+J-L-O@users.noreply.github.com>
2018-12-09 00:26:16 +0100

    Create the package setup #15 (#17)
    * Add alternative render mode
    
    * Change clipping behavior
    
    * Make sure the bounding box doesn't become larger than Pillow allows.
    
    * Make the environment an installable package.
    
    * Add missing commas
    
    * Fix spelling in README.md
    
    Co-Authored-By: J-L-O <18723283+J-L-O@users.noreply.github.com>
    
    * Remove useless comment
    
    Co-Authored-By: J-L-O <18723283+J-L-O@users.noreply.github.com>
    
    * Change version number to 0.1.0
    
    Co-Authored-By: J-L-O <18723283+J-L-O@users.noreply.github.com>
    
    * Add trailing blank lines to .gitignore, _init_.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+requirements = [
+      'gym',
+      'chainer',
+      'numpy',
+      'Pillow']
+
+setup(name='text_localization_environment',
+      version='0.1.0',
+      install_requires=requirements
+)
